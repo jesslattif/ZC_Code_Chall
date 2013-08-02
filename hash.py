9f88753b0417ff475ac1367c35d8d4a2eece59c5
Jessica <jesslattif@gmail.com>
2013-08-02 10:09:00 -0700

    adding some comments

--- hash.py
+++ hash.py
@@ -1,3 +1,4 @@
+# cURL from command line: curl --data "eat_more_lunch=yes_please" https://zerocater.com/web_dev_challenge/  -> returns git URL with message:
 """Take the hexadecimal digest of the MD5 hash of the string "All I really want to do is eat some pie" and enter it in the form on the original page."""
 
 import hashlib as h
@@ -7,7 +8,8 @@ message = h.md5("All I really want to do is eat some pie".encode("utf8"))
 print message.digest()
 
 print message.digest()
-# .decode("utf8")
+# .decode("utf8")? No, that doesn't work - 'utf8' codec can't decode byte 0xbf in position 2: invalid start byte
+
 
 print message.hexdigest()
 
